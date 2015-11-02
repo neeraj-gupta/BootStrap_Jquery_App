32294e758de3720a5568a73b842e1f90931f500a
neeraj-gupta <neeraj.gupta@sjsu.edu>
2015-11-01 16:15:04 -0800

    new pages and footer change

--- routes/index.js
+++ routes/index.js
@@ -5,48 +5,62 @@ var BlogSchema = mongoose.model("Blogs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index1', {title: 'My Website'});
-});
-
-router.get('/iot', function(req, res, next){
-  res.render('iot', {title: 'IoT Page'});
+  res.render('index', {title: 'My Website'});
 });
 
 router.get('/bigdata', function(req, res, next) {
     res.render('bigdata', {title: 'Big Data Page'});
 });
 
-router.get('/locations', function(req, res, next) {
-    res.render('locations', {title: 'My Location'});
+router.get('/blog', function(req, res, next) {
+    if(req.query.link) {
+        console.log(req.query.link);
+        BlogSchema.findOne({"link": req.query.link}, function(err, doc){
+            if(err){
+                console.log(err);
+            }
+            console.log(doc);
+            res.json(doc);
+        })
+    }
+    else{
+        res.render('blog', {title: 'Blog'});
+    }
+});
+
+router.get('/business', function(req, res, next) {
+    res.render('business', {title: 'Business Process'});
+});
+
+router.get('/code', function(req, res, next) {
+    res.render('code', {title: 'Code of conduct Page'});
 });
 
 router.get('/contacts', function(req, res, next) {
     res.render('contacts', {title: 'Contact'});
 });
 
+router.get('/iot', function(req, res, next){
+    res.render('iot', {title: 'IoT Page'});
+});
+
+router.get('/locations', function(req, res, next) {
+    res.render('locations', {title: 'My Location'});
+});
+
 router.get('/management', function(req, res, next) {
     res.render('management', {title: 'Management Page'});
+});
+
+router.get('/mobile', function(req, res, next) {
+    res.render('mobile', {title: 'Mobile Page'});
 });
 
 router.get('/process', function(req, res, next) {
     res.render('process', {title: 'Process Page'});
 });
 
-router.get('/blog', function(req, res, next) {
-	if(req.query.link) {
-		console.log(req.query.link);
-		BlogSchema.findOne({"link": req.query.link}, function(err, doc){
-			if(err){
-				console.log(err);
-			}
-			console.log(doc);
-			res.json(doc);
-		})
-	}
-	else{
-		res.render('blog', {title: 'Blog'});
-	}
-});
+
 
 
 module.exports = router;
